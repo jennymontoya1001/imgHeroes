609a4ad46de72f2b7c96caf3fe9646b2006ded02
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-07-01 17:41:08 -0500

    Create HeroCard.js

---
+++ HeroCard.js
@@ -0,0 +1,16 @@
+   <div className="card ms-3" style={{maxWidth: 540}}>
+            <div className="row no-gutters">
+                <div className="col-md-4">
+                    <img src="" className="card-img" alt=""/>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                      <h5 className="card-title"></h5>
+                      <p className="card-text"></p>
+                      <p className="card-text">
+                          <small className="text-muted"></small>
+                      </p>
+                  </div>
+                </div>
+            </div>    
+        </div>
