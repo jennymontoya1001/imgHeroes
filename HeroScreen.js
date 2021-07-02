b7fb4b1c015e57433e447c7cb352680194310064
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-07-01 19:00:02 -0500

    Create HeroScreen.js

---
+++ HeroScreen.js
@@ -0,0 +1,23 @@
+ <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={`../assets/heroes/${heroeId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
+                />
+            </div>
+
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3> {superhero} </h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b> Alter ego: </b> {alter_ego} </li>
+                    <li className="list-group-item"> <b> Publisher: </b> {publisher} </li>
+                    <li className="list-group-item"> <b> First appearance: </b> {first_appearance} </li>
+                </ul>
+
+                <h5> Characters </h5>
+                <p> {characters} </p>
+
+            </div>
+
+        </div>
