762cd59a110fa8f8a108686950601fa5848aa7ad
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-07-01 20:40:36 -0500

    Create SearchScreen.js

---
+++ SearchScreen.js
@@ -0,0 +1,31 @@
+   <div>
+            <div>
+                <h1>Search Screen</h1>
+                <hr />
+                <div className="row">
+                    <div className="col-5">
+                        <h4> Search Form </h4>
+                        <hr />
+                        <form>
+                            <input
+                                type="text"
+                                placeholder="Find your hero"
+                                className="form-control"
+                                name="searchText"
+                                autoComplete="off"
+                            />
+                            <button
+                                type="submit"
+                                className="btn m-1 btn-block btn-outline-primary"
+                            >
+                                Search...
+                            </button>
+                        </form>
+                    </div>
+                    <div className="col-7">
+                        <h4> Results </h4>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
